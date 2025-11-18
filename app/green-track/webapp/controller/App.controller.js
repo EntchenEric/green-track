c672103e4844db374ed79eae3228e9cd7cce3617
Eric Seidel <eseidel2004@gmail.com>
2025-11-18 12:38:10 +0000

    refactor sample data

--- app/green-track/webapp/controller/App.controller.js
+++ app/green-track/webapp/controller/App.controller.js
@@ -1,10 +1,16 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], (BaseController) => {
+  "sap/ui/core/mvc/Controller",
+  "sap/ui/core/UIComponent"
+], (BaseController, UIComponent) => {
   "use strict";
 
   return BaseController.extend("greentrack.controller.App", {
       onInit() {
+      },
+
+      onNavToDashboard() {
+          const oRouter = UIComponent.getRouterFor(this);
+          oRouter.navTo("Dashboard");
       }
   });
 });
